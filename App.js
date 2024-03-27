88b2cd8d1b22b506a163cba4fb6c774993530cf5
pallvi <pallvi.sharma@itechnolabs.tech>
2024-03-27 17:00:07 +0530

    react-native-splash-screen

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 
 
 import store from './src/store/store';
 import Firstrd from './src/screens/Firstrd';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+
+useEffect(() => {
+SplashScreen.hide()
+}, [])
+
+
   return (
     <Provider store={store}>
       <Firstrd />
